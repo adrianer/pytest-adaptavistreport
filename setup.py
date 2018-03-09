a7a14ca8cc63934206121cd7e7a9dcb50c10acbc
mabouelfadl <6151319+mabouelfadl@users.noreply.github.com>
2018-03-09 08:59:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='pytest-adaptivistreport',
-    description='pytest plugin that aims to report test cases results to Jiras plugin Adaptivist Test Management.',
+    name='pytest-adaptavistreport',
+    description='pytest plugin that aims to report test cases results to Jiras plugin Adaptavist Test Management.',
     long_description=open("README.md").read(),
     version='0.0.1',
-    url='https://github.com/mabouelfadl/pytest-adaptivistreport',
-    download_url='https://github.com/mabouelfadl/pytest-adaptivistreport/archive/0.1.tar.gz',
+    url='https://github.com/mabouelfadl/pytest-adaptavistreport',
+    download_url='https://github.com/mabouelfadl/pytest-adaptavistreport/archive/0.1.tar.gz',
     license='proprietary',
     author='Mariam Abouelfadl',
     author_email='mabouelfadl@gmail.com',
-    py_modules=['pytest_adaptivistreport'],
-    entry_points={'pytest11': ['adaptivistreport = pytest_adaptivistreport']},
+    py_modules=['pytest_adaptavistreport'],
+    entry_points={'pytest11': ['adaptavistreport = pytest_adaptavistreport']},
     platforms='any',
     install_requires=['pytest>=3.3.2'],
 )
